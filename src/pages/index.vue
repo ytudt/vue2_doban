e3219079fa2bdcd633f59e7b57717b8a0d556bc3
dt <st90@study.com>
2016-12-19 22:05:44 +0800

    加入电影分类

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <ul class="movieLists">
+  <ul class="movie-taps clr" >
+    <li class="movie-tap" v-bind:class="{active:item.isActive}" v-for="item in movieTypes" @click="getMovieList(item)">{{item.title}}</li>
+    <!-- <li class="movie-tap">即将上映</li>
+    <li class="movie-tap">口碑榜</li>
+    <li class="movie-tap">TOP250</li> -->
+  </ul>
+    <ul class="movieLists" v-show="!isLoading">
       <li class="movieList" v-for="item in list" track-by="$index">
         <router-link :to="{ path: 'movieDetail', query:{id:item.id}}">
           <div>
@@ -15,6 +21,25 @@
   </div>
 </template>
 <style>
+.movie-taps{
+  width: 100%;
+  height: 4rem;
+  /*margin-top:20px;*/
+}
+.movie-tap{
+  width: 33.3%;
+  float: left;
+  text-align: center;
+  height: 4rem;
+  line-height: 4rem;
+}
+/*.movie-tap:hover{
+
+}*/
+.movie-tap.active
+{
+  background: rgba(0,0,0,.5);
+}
 .movieLists {
   width: 100%;
   margin: 20px 0;
@@ -37,7 +62,7 @@
 import Loading from '../components/Loading.vue'
 import config from '../config'
 import { mapActions} from 'vuex'
-import {PULL_LIST} from '../store/modules/indexList'
+import {PULL_LIST,MODIFY_TAB} from '../store/modules/indexList'
 import {IS_LOADED,IS_LOADING} from '../store/index.js'
 import pullList from '../mixins/pullList'
 
@@ -49,7 +74,19 @@ export default {
   data() {
     return {
       // serverUrl:'this.$store.state.isLoading',
-      list: []
+      movieTypes:[{
+        title:'正在热映',
+        url:'/v2/movie/in_theaters',
+        // isActive:true
+      },{
+        title:'即将上映',
+        url:'/v2/movie/coming_soon'
+      },{
+        title:'TOP250',
+        url:'/v2/movie/top250'
+      }],
+      list: [],
+      downLoading:true
     }
   },
   computed:{
@@ -60,22 +97,56 @@ export default {
     // }
   },
   created() {
+    modifyTab(this.movieTypes,this.$store.state.indexList.activeTitle);
     var that = this;
       if (this.$store.state.indexList.list.length != 0) {
         this.list = this.$store.state.indexList.list;
         this.IS_LOADED();
+        this.downLoading=false;
       } else {
         this.$http.get(config.serverUrl + '/v2/movie/in_theaters').then((response) => {
           this.PULL_LIST(response.body.subjects);
           this.IS_LOADED();
           this.list = response.body.subjects;
           this.isLoading = false;
-
+          this.downLoading=false;
         }, (response) => {});
       }
   },
   methods: {
-    ...mapActions([PULL_LIST,IS_LOADED,IS_LOADING]),
+    ...mapActions([PULL_LIST,IS_LOADED,IS_LOADING,MODIFY_TAB]),
+    getMovieList(item){
+      if(item.isActive||this.downLoading){
+        return
+      }else{
+        this.downLoading=true;
+        this.MODIFY_TAB(item.title);
+        this.IS_LOADING();
+        for(let i=0;i<this.movieTypes.length;i++){
+          this.movieTypes[i].isActive=false;
+        }
+        item.isActive=true;
+        this.$http.get(config.serverUrl + item.url).then((response) => {
+          this.PULL_LIST(response.body.subjects);
+          this.IS_LOADED();
+          this.list = response.body.subjects;
+          this.isLoading = false;
+          this.downLoading=false;
+        }, (response) => {});
+      }
+
+    }
   }
+
+
 }
+ function modifyTab(arr,activeTitle){
+    for(let i=0;i<arr.length;i++){
+      if(arr[i].title===activeTitle){
+        arr[i].isActive=true;
+      }else{
+        arr[i].isActive=false;
+      }
+    }
+  }
 </script>
--- src/store/modules/indexList.js
+++ src/store/modules/indexList.js
@@ -1,22 +1,32 @@
 import Vue from 'vue'
 
 export const PULL_LIST = 'PULL_LIST' //登录成功
+export const MODIFY_TAB = 'MODIFY_TAB' //修改激活TAB
 // export const USER_SIGNOUT = 'USER_SIGNOUT' //退出登录
 
 export default {
     state: {
-        list:[]
+       activeTitle:'正在热映',
+       list:[]
     },
     mutations: {
         [PULL_LIST](state, list) {
             console.log(state)
             state.list=list;
             console.log(state)
+        },
+         [MODIFY_TAB](state, tabText) {
+            console.log(state)
+            state.activeTitle=tabText;
+            console.log(state)
         }
     },
     actions: {
         [PULL_LIST]({commit}, list) {
             commit(PULL_LIST, list)
+        },
+        [MODIFY_TAB]({commit},tabText){
+          commit(MODIFY_TAB, tabText)
         }
     }
 }
